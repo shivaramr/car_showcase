eb8b3901aa37b12d56c6eb20f5232b2425327fa5
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-09-22 10:37:30 +0530

    Components code formating

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,33 +1,32 @@
-"use client"
+"use client";
 
-import { ShowMoreProps } from '@/types'
-import { useRouter } from 'next/navigation'
-import React from 'react'
-import { CustomButton } from '.'
-import { updateSearchParams } from '@/utils'
+import { ShowMoreProps } from "@/types";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { CustomButton } from ".";
+import { updateSearchParams } from "@/utils";
 
-
-const ShowMore = ({isNext,pageNumber}:ShowMoreProps) => {
+const ShowMore = ({ isNext, pageNumber }: ShowMoreProps) => {
   const router = useRouter();
 
-  const handleNavigation = () => { 
-    const newLimit = (pageNumber+1)*10;
-    const newPathName = updateSearchParams("limit",`${newLimit}`)
-    router.push(newPathName)
-   }
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSearchParams("limit", `${newLimit}`);
+    router.push(newPathName);
+  };
 
-    return (
-    <div className='w-full flex-center gap-5 mt-10'>
-        {!isNext && (
-            <CustomButton 
-            title="Show More"
-            btnType='button'
-            containerStyles='bg-primary-blue rounded-full text-white'
-            handleClick={handleNavigation}
-            />
-        )}
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show More"
+          btnType="button"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ShowMore
+export default ShowMore;
